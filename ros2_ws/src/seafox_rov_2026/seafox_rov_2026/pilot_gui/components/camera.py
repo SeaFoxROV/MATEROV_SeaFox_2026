46822590f492fb4a3fd923175aaf144695bc2a3e
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-02-18 14:57:57 -0800

    feat: add placeholder for cameras

--- ros2_ws/src/seafox_rov_2026/seafox_rov_2026/pilot_gui/components/camera.py
+++ ros2_ws/src/seafox_rov_2026/seafox_rov_2026/pilot_gui/components/camera.py
@@ -1,15 +1,32 @@
-from PyQt5.QtWidgets import QFrame, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QFrame, QHBoxLayout, QVBoxLayout, QLabel
 from PyQt5.QtCore import Qt
 
 
 class CameraWidget(QFrame):
     def __init__(self):
         super().__init__()
+        self.cameras = {
+            "upper":"http://admin:admin@192.168.1.9:6688/snapshot/PROFILE_000",
+            "middle":"http://admin:admin@192.168.1.4:6688/snapshot/PROFILE_000",
+            # "bottom":"http://admin:admin@192.168.1.4:6688/snapshot/PROFILE_000"
+        }
+
         self.setFrameShape(QFrame.StyledPanel)
 
-        layout = QHBoxLayout()
-        label = QLabel("Camera Widget")
-        label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(label)
+        main_layout = QVBoxLayout()
 
-        self.setLayout(layout)
+        for name, url in self.cameras.items():
+            camera_layout = QVBoxLayout()
+
+            title = QLabel(name.upper())
+            title.setAlignment(Qt.AlignCenter)
+
+            image_label = QLabel("Stream")
+            image_label.setAlignment(Qt.AlignCenter)
+
+            camera_layout.addWidget(title)
+            camera_layout.addWidget(image_label)
+
+            main_layout.addLayout(camera_layout)
+
+        self.setLayout(main_layout  )
