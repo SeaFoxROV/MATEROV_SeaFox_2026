fd9dfafca49cf728ba5c2d20e224ffdaba05d56f
Zaracla2112 <ever.alcaraz@cetys.edu.mx>
2026-03-07 14:25:21 -0800

    feat: added rviz launcher file and everything ready for sensor integration and force visualization

--- ros2_ws/src/seafox_sim_2026/seafox_sim_2026/depth_tf_node.py
+++ ros2_ws/src/seafox_sim_2026/seafox_sim_2026/depth_tf_node.py
@@ -33,7 +33,7 @@ class DepthTFNode(Node):
 
         t.header.stamp = self.get_clock().now().to_msg()
         t.header.frame_id = "world"
-        t.child_frame_id = "base_link"   # 🔥 CAMBIADO AQUÍ
+        t.child_frame_id = "base_link"   
 
         t.transform.translation.x = 0.0
         t.transform.translation.y = 0.0
