e6a74b0342778d26e60da1233acefa74b2baa5ad
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-03-28 11:38:18 -0700

    chore: comment unused widget

--- ros2_ws/src/seafox_rov_2026/seafox_rov_2026/pilot_gui/main_window.py
+++ ros2_ws/src/seafox_rov_2026/seafox_rov_2026/pilot_gui/main_window.py
@@ -26,40 +26,47 @@ class MainWindow(QMainWindow):
         self.right_widget = ModelWidget()
 
         layout.addWidget(self.left_widget)
-        layout.addWidget(self.right_widget)
+        # layout.addWidget(self.right_widget)
 
         layout.setStretch(0, 1)
         layout.setStretch(1, 1)
 
-
     def keyPressEvent(self, event):
         # Camera selection
-        if event.text() == '1':
+        if event.text() == "1":
             if self.selected_camera == "1":
                 self.left_widget.maximize_camera("main")
             self.selected_camera = "1"
             self.left_widget.select_camera("main")
             self.all_cameras = False
-            self.left_widget.set_title_style("main", "font-weight: bold; color: white; background-color: #2BCF47;")
-        elif event.text() == '2':
+            self.left_widget.set_title_style(
+                "main", "font-weight: bold; color: white; background-color: #2BCF47;"
+            )
+        elif event.text() == "2":
             if self.selected_camera == "2":
                 self.left_widget.maximize_camera("upper")
             self.selected_camera = "2"
             self.left_widget.select_camera("upper")
             self.all_cameras = False
-            self.left_widget.set_title_style("upper", "font-weight: bold; color: white; background-color: #2BCF47;")
-        elif event.text() == '3':
+            self.left_widget.set_title_style(
+                "upper", "font-weight: bold; color: white; background-color: #2BCF47;"
+            )
+        elif event.text() == "3":
             if self.selected_camera == "3":
                 self.left_widget.maximize_camera("middle")
             self.selected_camera = "3"
             self.left_widget.select_camera("middle")
             self.all_cameras = False
-            self.left_widget.set_title_style("middle", "font-weight: bold; color: white; background-color: #2BCF47;")
-        elif event.text() == '0':
+            self.left_widget.set_title_style(
+                "middle", "font-weight: bold; color: white; background-color: #2BCF47;"
+            )
+        elif event.text() == "0":
             self.selected_camera = 0
             self.all_cameras = True
             for name in self.left_widget.threads.keys():
-                self.left_widget.set_title_style(name, "font-weight: bold; color: white; background-color: #333;")
+                self.left_widget.set_title_style(
+                    name, "font-weight: bold; color: white; background-color: #333;"
+                )
         elif event.key() == Qt.Key_Escape:
             self.left_widget.reset_camera_view()
 
@@ -72,18 +79,18 @@ class MainWindow(QMainWindow):
                 # self.left_widget.get_snapshot()
 
         # Crab detection
-        elif event.text() == 'D':
+        elif event.text() == "D":
             if self.selected_camera:
                 for thread in self.left_widget.threads.values():
                     print(self.left_widget.selected_camera)
                     thread.start_detection(self.left_widget.selected_camera)
-        
-        elif event.text() == 'd':
+
+        elif event.text() == "d":
             for thread in self.left_widget.threads.values():
                 thread.stop_detection()
 
         # Quick conf
-        elif event.text() == 'B':
+        elif event.text() == "B":
             if self.all_cameras:
                 for name in self.left_widget.threads.keys():
                     adjuster = self.left_widget.threads[name].adjuster
@@ -92,7 +99,7 @@ class MainWindow(QMainWindow):
                 adjuster = self.left_widget.get_selected_camera()
                 if adjuster:
                     adjuster.set_brightness(adjuster.brightness + 10)
-        elif event.text() == 'b':
+        elif event.text() == "b":
             if self.all_cameras:
                 for name in self.left_widget.threads.keys():
                     adjuster = self.left_widget.threads[name].adjuster
@@ -101,7 +108,7 @@ class MainWindow(QMainWindow):
                 adjuster = self.left_widget.get_selected_camera()
                 if adjuster:
                     adjuster.set_brightness(adjuster.brightness - 10)
-        elif event.text() == 'C':
+        elif event.text() == "C":
             if self.all_cameras:
                 for name in self.left_widget.threads.keys():
                     adjuster = self.left_widget.threads[name].adjuster
@@ -110,7 +117,7 @@ class MainWindow(QMainWindow):
                 adjuster = self.left_widget.get_selected_camera()
                 if adjuster:
                     adjuster.set_contrast(adjuster.contrast + 0.1)
-        elif event.text() == 'c':
+        elif event.text() == "c":
             if self.all_cameras:
                 for name in self.left_widget.threads.keys():
                     adjuster = self.left_widget.threads[name].adjuster
@@ -119,7 +126,7 @@ class MainWindow(QMainWindow):
                 adjuster = self.left_widget.get_selected_camera()
                 if adjuster:
                     adjuster.set_contrast(adjuster.contrast - 0.1)
-        elif event.text() == 'S':
+        elif event.text() == "S":
             if self.all_cameras:
                 for name in self.left_widget.threads.keys():
                     adjuster = self.left_widget.threads[name].adjuster
@@ -128,7 +135,7 @@ class MainWindow(QMainWindow):
                 adjuster = self.left_widget.get_selected_camera()
                 if adjuster:
                     adjuster.set_saturation(adjuster.saturation + 0.1)
-        elif event.text() == 's':
+        elif event.text() == "s":
             if self.all_cameras:
                 for name in self.left_widget.threads.keys():
                     adjuster = self.left_widget.threads[name].adjuster
@@ -137,6 +144,7 @@ class MainWindow(QMainWindow):
                 adjuster = self.left_widget.get_selected_camera()
                 if adjuster:
                     adjuster.set_saturation(adjuster.saturation - 0.1)
+
 
 def main():
     app = QApplication(sys.argv)
