91f3b46a0a934c311969a8eb1a45d1ca2f2e9748
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-06-09 16:39:03 -0700

    chore(merge): add merged files

--- copilot_ws/src/gui/main.py
+++ copilot_ws/src/gui/main.py
@@ -1,17 +1,18 @@
+from components.crabVision import crabVision
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget
 
-from components.crabVision import crabVision
-from components.iceberg import Iceberg
+
 from components.photogrammetry import Photogrammetry
-from components.percentage_species import EDNA
+from components.iceberg import Iceberg
+from components.eDNA import eDNA
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Copilot GUI")
+        self.setWindowTitle("Mi Template PyQt5")
         self.resize(900, 600)
 
         self.tabs = QTabWidget()
@@ -19,7 +20,7 @@ class MainWindow(QMainWindow):
         self.tabs.addTab(Photogrammetry(), "Photogrammetry")
         self.tabs.addTab(Iceberg(), "Iceberg")
         self.tabs.addTab(crabVision(), "Crab Vision")
-        self.tabs.addTab(EDNA(), "eDNA")
+        self.tabs.addTab(eDNA(), "eDNA")
 
         self.setCentralWidget(self.tabs)
 
